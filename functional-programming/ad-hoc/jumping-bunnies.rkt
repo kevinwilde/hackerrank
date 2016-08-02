e8576676e6055d5c1e6aa577fcb89b57d52937d9
Kevin Wilde <kevinwilde2018@u.northwestern.edu>
2016-08-02 17:16:38 -0400

    Update jumping-bunnies.rkt

--- functional-programming/ad-hoc/jumping-bunnies.rkt
+++ functional-programming/ad-hoc/jumping-bunnies.rkt
@@ -6,16 +6,15 @@
   (map string->number (string-split (read-line))))
 
 (define (lcm-list lst)
-  (let ([n (length lst)])
-    (cond [(< n 2) 0]
-          [(= n 2) (lcm (first lst) (list-ref lst 1))]
-          [else    (lcm (first lst) (lcm-list (rest lst)))])))
+  (cond [(empty? lst) +nan.0]
+        [else (foldl lcm (first lst) (rest lst))]))
 
 (define _ (read-line)) ; unused
 (display (lcm-list (read-list)))
 
 ;; Unit Tests
 (require rackunit)
+(check-eq? (lcm-list empty) +nan.0)
 (check-eq? (lcm-list (list 10 25 75))  150)
 (check-eq? (lcm-list (list 1 10 17 5)) 170)
 (check-eq? (lcm-list (list 7 49))  49)
