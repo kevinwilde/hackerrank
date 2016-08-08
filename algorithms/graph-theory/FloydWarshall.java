54d62a87040cb32c180be3968fc235a77b792179
Kevin Wilde <kevinwilde2018@u.northwestern.edu>
2016-08-08 08:58:22 -0400

    Update FloydWarshall.java

--- algorithms/graph-theory/FloydWarshall.java
+++ algorithms/graph-theory/FloydWarshall.java
@@ -2,6 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        
+        Edge[] edges = new Edge[m];
+        for (int i = 0; i < m; i++) {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            int r = scan.nextInt();
+            edges[i] = new Edge(x-1, y-1, r);
+        }
+        
+        int[][] dists = floydWarshall(n, edges);
+        int q = scan.nextInt();
+        
+        for (int i = 0; i < q; i++) {
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int d = dists[a-1][b-1];
+            if (d < Integer.MAX_VALUE) {
+                System.out.println(d);
+            } else {
+                System.out.println(-1);
+            }
+        }
+    }
     
     private static int[][] floydWarshall(int n, Edge[] edges) {
         int[][] dists = new int[n][n];
@@ -30,34 +58,6 @@ public class Solution {
             }
         }
         return dists;
-    }
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int m = scan.nextInt();
-        
-        Edge[] edges = new Edge[m];
-        for (int i = 0; i < m; i++) {
-            int x = scan.nextInt();
-            int y = scan.nextInt();
-            int r = scan.nextInt();
-            edges[i] = new Edge(x-1, y-1, r);
-        }
-        
-        int[][] dists = floydWarshall(n, edges);
-        int q = scan.nextInt();
-        
-        for (int i = 0; i < q; i++) {
-            int a = scan.nextInt();
-            int b = scan.nextInt();
-            int d = dists[a-1][b-1];
-            if (d < Integer.MAX_VALUE) {
-                System.out.println(d);
-            } else {
-                System.out.println(-1);
-            }
-        }
     }
 }
 
